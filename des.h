6f51a640c399cad4d67aaae85b6b23515f1176c1
Jimmy12345 <2362763425@qq.com>
2023-03-23 00:20:00 +0800

    定义了部分结构

---
+++ des.h
@@ -0,0 +1,16 @@
+//
+// Created by Jimmy on 2023/3/23.
+//
+
+#ifndef DES_DES_H
+#include "iostream"
+
+#define DES_DES_H
+#define MAX 100
+class des{
+    //todo 类的成员申明
+};
+//todo 各类数组（表）的定义
+
+#endif //DES_DES_H
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include "des.h"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
